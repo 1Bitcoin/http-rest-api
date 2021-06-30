918a0ebc3d0c78364d020a6f6b3b261a981d5477
Дмитрий l W.W <shigalkin@mail.ru>
2021-06-30 23:29:24 +0300

    process

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,10 +1,15 @@
 package apiserver
 
+import "http-rest-api/internal/store"
+
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level""`
+	Store *store.Config
 }
 
 func NewConfig() *Config {
-	return &Config{}
+	return &Config{
+		Store: store.NewConfig()
+	}
 }
