eb7faf785672482aba676606021c753f618ee5b8
1Bitcoin <shigalkin@mail.ru>
2021-07-06 17:04:33 +0300

    add repository database

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,8 +1,11 @@
 package apiserver
 
+import "github.com/1Bitcoin/http-rest-api/internal/app/store"
+
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level""`
+	Store    *store.Config
 }
 
 func NewConfig() *Config {
